da500ca3808b7860575dadfa89eede4957c955c7
Apprentice <apprentice@dbc25.local>
2013-05-31 13:47:27 -0700

    Add View.display_list_cleared

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,12 +1,13 @@
 class View
 
   def self.display_list_cleared
+    puts "All tasks deleted. Let's start a fresh list!" 
   end
 
-  def self.display_added_message
+  def self.display_added_message(task)
   end
 
-  def self.display_deleted_message
+  def self.display_deleted_message(task)
   end
 
   def self.display_list(tasks)
