bea2e54357e0e7d39c107c29092e0f9e43f6adbf
Apprentice <apprentice@dbc25.local>
2013-05-31 15:20:47 -0700

    Finish controller model

--- app/controllers/todocontroller.rb
+++ app/controllers/todocontroller.rb
@@ -3,11 +3,23 @@ class Controller
 
 
   def run
-  end
 
-  def parse_argv
-    # map argv to method and arguement
-    # map method to controller methods and pass in arguement
+    # method = ARGV.shift 
+    # *arg = ARGV.join(" ")
+
+    method, *arg = ARGV
+
+    case method
+
+    when "list"
+      display_list
+    when "add"
+      create_task(arg.join(" "))
+    when "delete"
+      delete_task(arg[0])
+    when "complete"
+      mark_as_complete(arg[0])
+    end
   end
 
   def display_list
@@ -15,17 +27,21 @@ class Controller
   end
 
   def create_task(task_description)
+    Task.create({ text: task_description })
   end
 
   def delete_task(display_number)
+    Task.find_by_display_number(display_number).destroy
   end
 
   def mark_as_complete(display_number)
+    Task.find_by_display_number(display_number).mark_completed!
   end
 
   def clear_list
     Task.destroy_all
-    display_list
+    View.display_list_cleared
   end
 
 end
+
